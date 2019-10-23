e83778e6a5fcd9cbfb94fa7a74eaa5d774f97f84
Pale-Blue-Dot-97 <54509156+Pale-Blue-Dot-97@users.noreply.github.com>
2019-10-23 18:01:25 +0100

    Added calc_variances() to remove noisy data
    Currently gets stuck on deleting every point after the first noisy point is deleted.

--- Starting/starting_point.py
+++ Starting/starting_point.py
@@ -28,12 +28,28 @@ data = pd.read_table('S3_1_92S.TAB', delimiter=',', names=names, na_values=-9999
 
 data_columns = ['BR', 'BTH', 'BPH', 'BMAG', 'AVG_BMAG', 'DELTA', 'LAMBDA', 'RMS_BR', 'RMS_BTH', 'RMS_BPH', 'NUM_PTS']
 
-# Find missing data points
-for i in data_columns:
-    for row in data[i]:
-        if row < -9999.0:
-            print('Delete')
-            data[i].__delitem__(row)
+
+def calc_variances(data):
+    deleted = []
+    for i in data_columns:
+        data_min = np.min(data[i])
+        data_max = np.max(data[i])
+        max_var = 0.5 * np.abs(data_max - data_min)
+        for j in range(len(data[i])):
+            if j != 0:
+                this_point = data[i][j]
+                k = j-1
+                while k in deleted:
+                    k = k-1
+
+                last_point = data[i][k]
+                var = np.abs(this_point - last_point)
+                if var > max_var:
+                    print('Delete %d' % j)
+                    print(this_point)
+                    print(last_point)
+                    deleted.append(j)
+                    data.drop(j, axis=0, inplace=True)
 
 # Interpolate missing data
 # ii = np.arange(1,len(data),1)
@@ -41,16 +57,24 @@ for i in data_columns:
 
 # datetime.datetime.strptime(time_string, '%Y-%m-DT%h:%....')
 
-plt.subplot(4, 2, 1)
-plt.plot(data['BR'])
-plt.ylabel('B_r [nT]')
-plt.subplot(4, 2, 2)
-plt.plot(data['BTH'])
-plt.ylabel('B_th [nT]')
-plt.subplot(4, 2, 3)
-plt.plot(data['BPH'])
-plt.ylabel('B_ph [nT]')
-plt.subplot(4, 2, 4)
-plt.plot(data['BMAG'])
-plt.ylabel('|B| [nT]')
-plt.show()
+
+def main():
+    calc_variances(data)
+
+    plt.subplot(4, 2, 1)
+    plt.plot(data['BR'])
+    plt.ylabel('B_r [nT]')
+    plt.subplot(4, 2, 2)
+    plt.plot(data['BTH'])
+    plt.ylabel('B_th [nT]')
+    plt.subplot(4, 2, 3)
+    plt.plot(data['BPH'])
+    plt.ylabel('B_ph [nT]')
+    plt.subplot(4, 2, 4)
+    plt.plot(data['BMAG'])
+    plt.ylabel('|B| [nT]')
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
