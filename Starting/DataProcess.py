041ed601a1f3ed9bf245d699047b74b5ef00c867
Pale-Blue-Dot-97 <54509156+Pale-Blue-Dot-97@users.noreply.github.com>
2019-12-11 12:30:53 +0000

    Added loading of data and main() to DataProcess.py

--- Starting/DataProcess.py
+++ Starting/DataProcess.py
@@ -24,3 +24,40 @@ import pyttsx3 as speech
 # =====================================================================================================================
 #                                                     METHODS
 # =====================================================================================================================
+
+
+def load_data():
+    """Load in cleaned and normalised data from file
+    """
+
+    data_names = ['BR', 'BTH', 'BPH', 'BMAG', 'UNIX TIME', 'BR_norm', 'BTH_norm', 'BPH_norm']
+
+    data = pd.read_csv('VOY2_JE_PROC.csv', names=data_names)
+
+    return data
+
+
+# =====================================================================================================================
+#                                                       MAIN
+# =====================================================================================================================
+def main():
+    engine = speech.init()
+
+    engine.say("Loading data")
+    engine.runAndWait()
+
+    data = load_data()
+
+    # Alert bell
+    for i in range(1, 4):
+        sys.stdout.write('\r\a')
+        sys.stdout.flush()
+        time.sleep(1)
+    sys.stdout.write('\n')
+
+    engine.say("Finished")
+    engine.runAndWait()
+
+
+if __name__ == '__main__':
+    main()
