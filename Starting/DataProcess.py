74daf895e32f9038632ae47d0a4b0b6342429f4f
Pale-Blue-Dot-97 <54509156+Pale-Blue-Dot-97@users.noreply.github.com>
2019-12-16 12:10:16 +0000

    Added random block creation

--- Starting/DataProcess.py
+++ Starting/DataProcess.py
@@ -42,6 +42,25 @@ def load_data():
     return data, norm_data
 
 
+def renormalise(data):
+    """Re-normalises data from -1<x<1 to 0<x<1
+
+    Args:
+        data (DataFrame): Table containing data normalised to -1<x<1
+
+    Returns:
+        data (DataFrame): Data re-normalised to 0<x<1
+
+    """
+
+    data = data.copy()
+
+    for i in ['BR_norm', 'BPH_norm', 'BTH_norm', 'BMAG_norm']:
+        data[i] = (data[i] + 1.0).multiply(0.5)
+
+    return data
+
+
 def create_blocks(data):
     """Splits the data into 4096 long blocks as numpy arrays
 
@@ -52,17 +71,41 @@ def create_blocks(data):
         blocks ([[[float]]]): 3D array containing blocks of 4096 * 4 values
 
     """
-    data = data.copy()
-
-    # Re-normalises data from -1<x<1 to 0<x<1
-    for i in ['BR_norm', 'BPH_norm', 'BTH_norm', 'BMAG_norm']:
-        data[i] = (data[i] + 1.0).multiply(0.5)
 
     blocks = []
 
     # Slices DataFrame into 4096 long blocks
     for i in range(int(len(data['BR_norm']) / 4096.0)):
         block_slice = data[(i-1) * 4096: (i * 4096) - 1]
+
+        block = []
+
+        for j in ['BR_norm', 'BPH_norm', 'BTH_norm', 'BMAG_norm']:
+            block.append(np.array(block_slice[j]))
+
+        blocks.append(np.array(block))
+
+    return blocks
+
+
+def create_random_blocks(data, n):
+    """Selects n number of random 4096 long blocks from the data as numpy arrays
+
+        Args:
+            data (DataFrame): Table containing data to split into blocks
+            n (int): Number of blocks to randomly select
+
+        Returns:
+            blocks ([[[float]]]): 3D array containing blocks of 4096 * 4 values
+
+    """
+
+    blocks = []
+
+    # Slices DataFrame into 4096 long blocks
+    for i in range(n):
+        j = random.choice(np.linspace(0, len(data['BR_norm'] - 4096), 1))
+        block_slice = data[j : j + 4096]
 
         block = []
 
@@ -172,6 +215,11 @@ def main():
     engine.runAndWait()
 
     data, norm_data = load_data()
+
+    engine.say("Re-normalising data")
+    engine.runAndWait()
+
+    norm_data = renormalise(norm_data)
 
     engine.say("Perturbing data")
     engine.runAndWait()
