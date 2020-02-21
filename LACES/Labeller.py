b4679b220cb49f4ad7721ab06be3b619d00b2f6b
Pale-Blue-Dot-97 <54509156+Pale-Blue-Dot-97@users.noreply.github.com>
2020-02-21 13:49:04 +0000

    Added TODOs to Labeller.py

---
+++ LACES/Labeller.py
@@ -0,0 +1,8 @@
+"""Script to manually label Voyager 1 Jupiter flyby magnetometer data for neural network training
+
+TODO:
+    * Import Voyager 1 data and plot against data index and datetime
+    * Manually define CSCs and output to file for data labelling
+    * Add further science classifications
+
+"""
