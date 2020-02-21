ba7f2aacd2eb3486d2f71e1488fbc8438b2b4235
Pale-Blue-Dot-97 <54509156+Pale-Blue-Dot-97@users.noreply.github.com>
2020-02-21 13:51:42 +0000

    Added imports, data loading and main to Labeller.py

--- LACES/Labeller.py
+++ LACES/Labeller.py
@@ -6,3 +6,49 @@ TODO:
     * Add further science classifications
 
 """
+
+# =====================================================================================================================
+#                                                     IMPORTS
+# =====================================================================================================================
+import time
+import sys
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.signal as sg
+import random
+
+
+# =====================================================================================================================
+#                                                     METHODS
+# =====================================================================================================================
+def load_data():
+    """Load in cleaned and normalised data from file
+
+    Returns:
+        data (DataFrame): Table of all the cleaned and normalised data from file
+        norm_data (DataFrame): Table of just the normalised data
+
+    """
+
+    data_names = ['BR', 'BTH', 'BPH', 'BMAG', 'UNIX TIME', 'BR_norm', 'BTH_norm', 'BPH_norm', 'BMAG_norm']
+
+    data = pd.read_csv('VOY2_JE_PROC.csv', names=data_names, dtype=float, header=0)
+
+    norm_data = data.drop(columns=['BR', 'BTH', 'BPH', 'BMAG'])
+
+    norm_data.reset_index(drop=True)
+    data.reset_index(drop=True)
+
+    return data, norm_data
+
+
+# =====================================================================================================================
+#                                                       MAIN
+# =====================================================================================================================
+def main():
+    return
+
+
+if __name__ == '__main__':
+    main()
